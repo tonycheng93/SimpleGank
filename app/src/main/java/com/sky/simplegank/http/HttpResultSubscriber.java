8940d3bd69af3816c6554538a0d9ec6f00761141
tonycheng93 <tonycheng93@outlook.com>
2016-10-14 18:07:45 +0800

    defines the network callback interface

---
+++ app/src/main/java/com/sky/simplegank/http/HttpResultSubscriber.java
@@ -0,0 +1,37 @@
+package com.sky.simplegank.http;
+
+import rx.Subscriber;
+
+/**
+ * Created by tonycheng on 2016/10/14.
+ */
+
+public abstract class HttpResultSubscriber<T> extends
+        Subscriber<HttpResult<T>> {
+    protected HttpResultSubscriber() {
+        super();
+    }
+
+    @Override
+    public void onNext(HttpResult<T> tHttpResult) {
+        if (tHttpResult.isError()) {
+            onSuccess(tHttpResult);
+        } else {
+            onFail(new Throwable("error = " + tHttpResult.isError()));
+        }
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        onFail(e);
+    }
+
+    @Override
+    public void onCompleted() {
+        // TODO: 2016/10/14  onCompleted()方法中还不确定具体要封装些什么
+    }
+
+    public abstract void onSuccess(HttpResult<T> result);
+
+    public abstract void onFail(Throwable e);
+}
